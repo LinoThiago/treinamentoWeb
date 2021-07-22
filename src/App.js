71c771adcb0d00c2f925fffba46e3996b9577dd7
Joao Guilherme Arantes Freitas <joao.g.freitas@estudante.firjan.senai.br>
2021-07-22 15:56:47 -0300

    pagina pesquisa

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import PesquisaColaborador from './pages/PesquisaColaborador'
 import CadastrarColaboradores from './pages/CadastrarColaboradores';
+import Colaborador from './pages/Colaborador';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path="/home" exact component={Home}/>
       <Route path="/pesquisacolaborador" exact component={PesquisaColaborador}/>
       <Route path="/cadastrarcolaboradores" exact component={CadastrarColaboradores}/>
+      <Route path="/colaborador" exact component={Colaborador}/>
       </Container>
       </Switch>
       </BrowserRouter>
---
+++ src/pages/Colaborador/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Principal = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+`;
+export const CardColaborador = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+`;
